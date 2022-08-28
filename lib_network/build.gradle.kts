2065468e17ec64170a261888c422ae544e7135ab
jiayuxi <yuxi_jj@163.com>
2022-08-28 17:29:08 +0800

    网络 引用

---
+++ lib_network/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = VersionConfig.compileSdk
+
+    defaultConfig {
+        minSdk = VersionConfig.minSdk
+        targetSdk = VersionConfig.targetSdk
+        testInstrumentationRunner = VersionConfig.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(AndroidX.coreKtx)
+    implementation(Kotlin.Coroutine.core)
+    implementation(Kotlin.Coroutine.coreAndroid)
+    //
+    implementation(ThirdPart.OkHttp.okhttp)
+    implementation(ThirdPart.OkHttp.loggingInterceptor)
+    //
+    implementation(ThirdPart.Retrofit.retrofit)
+    implementation(ThirdPart.Retrofit.convertGson)
+    implementation(ThirdPart.Retrofit.adapterRxjava3)
+    //
+    implementation(ThirdPart.RxJava3.rxJava)
+    implementation(ThirdPart.RxJava3.rxAndroid)
+    //
+    testImplementation(Testing.testJunit)
+    androidTestImplementation(Testing.testJunit)
+    androidTestImplementation(Testing.espresso)
+}
